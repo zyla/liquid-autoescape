a1141454613c884ce6fe5676ebfb0e08d9508868
Justin Locsei <justin.locsei@gmail.com>
2015-05-10 16:44:16 -0400

    Move the logic for getting a Liquid variable name into a private method

--- lib/liquid/autoescape/template_variable.rb
+++ lib/liquid/autoescape/template_variable.rb
@@ -8,28 +8,44 @@ module Liquid
       # @return [Array<Symbol>] The filters applied to the variable
       attr_reader :filters
 
-      # Create a wrapper for a Liquid variable instance
-      #
-      # This normalizes the variable's information, since Liquid 2 and 3 handle
-      # variable names using different data structures.
-      #
-      # @param [Liquid::Variable] variable A Liquid variable as used in a template
-      # @return [Liquid::Autoescape::TemplateVariable]
-      def self.from_liquid_variable(variable)
-        lookup_name = variable.name.instance_variable_get("@name")
-        if lookup_name
+      class << self
+
+        # Create a wrapper for a Liquid variable instance
+        #
+        # This normalizes the variable's information, since Liquid 2 and 3 handle
+        # variable names using different data structures.
+        #
+        # @param [Liquid::Variable] variable A Liquid variable as used in a template
+        # @return [Liquid::Autoescape::TemplateVariable]
+        def from_liquid_variable(variable)
+          name = normalize_variable_name(variable)
+          filters = variable.filters.map { |f| f.first.to_sym }
+
+          new(:name => name, :filters => filters)
+        end
+
+      private
+
+        # Normalize the name of a Liquid variable
+        #
+        # Liquid 2 exposes the full variable name directly on the
+        # `Liquid::Variable` instance, while Liquid 3 manages it via a
+        # `Liquid::VariableLookup` instance that tracks both the base name and
+        # any lookup paths involved.
+        #
+        # @param [Liquid::Variable] variable A Liquid variable as used in a template
+        # @return [String] The name of the Liquid variable
+        def normalize_variable_name(variable)
+          lookup_name = variable.name.instance_variable_get("@name")
+          return variable.name unless lookup_name
+
           parts = [lookup_name]
           variable.name.instance_variable_get("@lookups").each do |lookup|
             parts << lookup
           end
-          name = parts.join(".")
-        else
-          name = variable.name
+          parts.join(".")
         end
 
-        filters = variable.filters.map { |f| f.first.to_sym }
-
-        new(:name => name, :filters => filters)
       end
 
       # Create a new wrapper around a Liquid variable used in a template
